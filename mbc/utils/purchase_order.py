4a71edb923eaf331d5dcf7c71bab92d573959ea0
Revasiya <87364473+Revasiya@users.noreply.github.com>
2023-03-05 16:13:39 +0530

    Update: code and set validation on make sales order

--- mbc/utils/purchase_order.py
+++ mbc/utils/purchase_order.py
@@ -1,17 +1,22 @@
-import datetime
 import frappe
 from frappe.model.mapper import get_mapped_doc
 from frappe.utils.data import cint, flt
 
 
+
 @frappe.whitelist()
 def make_sales_order(source_name, target_doc=None):
+    # This function takes in a Purchase Order name as `source_name`, and an optional target document object `target_doc`
+
+    # Call the `_make_sales_order` function and pass in the `source_name` and `target_doc` arguments
     return _make_sales_order(source_name, target_doc)
 
 
 def _make_sales_order(source_name, target_doc=None, ignore_permissions=False):
-    doc = frappe.get_doc('Purchase Order', source_name)
-    customer = frappe.db.get_value('Company', {'name': doc.company}, ['mbc_internal_customer'])
+    # This is a helper function that creates a Sales Order from a Purchase Order
+    
+    po = frappe.get_doc('Purchase Order', source_name)
+    customer = frappe.db.get_value('Company', {'name': po.company}, ['mbc_internal_customer'])
     customer_name = frappe.db.get_value('Customer', {'name': customer}, ['customer_name'])
     tax_category = frappe.db.get_value('Customer', {'name': customer}, ['tax_category'])
 
@@ -19,8 +24,8 @@ def _make_sales_order(source_name, target_doc=None, ignore_permissions=False):
     tax_temp_name = frappe.db.get_value('Sales Taxes and Charges Template', {'tax_category': tax_category}, ['name'])
 
     # get value for mbc_internal_company and mbc_default_po_warehouse
-    mbc_internal_company = frappe.db.get_value('Supplier', {'name': doc.supplier}, ['mbc_internal_company'])
-    mbc_default_po_warehouse = frappe.db.get_value('Supplier', {'name': doc.supplier}, ['mbc_default_po_warehouse'])
+    mbc_internal_company = frappe.db.get_value('Supplier', {'name': po.supplier}, ['mbc_internal_company'])
+    mbc_default_po_warehouse = frappe.db.get_value('Supplier', {'name': po.supplier}, ['mbc_default_po_warehouse'])
 
     # get value for sales payment_terms
     payment_terms = frappe.db.get_value('Customer', {'name': customer}, ['payment_terms'])
@@ -29,54 +34,62 @@ def _make_sales_order(source_name, target_doc=None, ignore_permissions=False):
     terms_name = frappe.db.get_value('Terms and Conditions', {'selling': 1, 'disabled': 0}, ['name'])
     terms_description = frappe.db.get_value('Terms and Conditions', {'name': terms_name}, ['terms'])
 
+    # check if customer has outstanding amount
+    outstanding_amount = 0.0
+    payment_entries = frappe.get_all('Payment Entry', filters={'party': customer, 'docstatus': 1})
+    
+    for pe in payment_entries:
+        out_std = frappe.db.get_value('Payment Entry Reference', {'parent': pe.name}, ['outstanding_amount'])
+        outstanding_amount += out_std
+
+    if outstanding_amount > 0.0:
+        frappe.msgprint(f"Customer {customer_name} has an outstanding amount of {outstanding_amount}. Please settle before creating a sales order.")
+        return
+
+    
+
     # set missing values
-    def set_missing_values(source, target):
-        if customer:
-            target.customer = customer
-            target.customer_name = customer_name
-
-        target.set_warehouse = mbc_default_po_warehouse
-        target.po_no = doc.name
-        target.po_date = doc.transaction_date
-        target.company = mbc_internal_company
-        target.payment_terms_template = payment_terms
-        target.tax_category = tax_category
-        target.taxes_and_charges = tax_temp_name
-        target.tc_name = terms_name
-        target.inter_company_order_reference = ""
-        target.terms = terms_description
-        target.flags.ignore_permissions = ignore_permissions
-        target.run_method("set_missing_values")
-        target.run_method("calculate_taxes_and_totals")
-
+    so = frappe.new_doc('Sales Order')
+    so.customer = customer
+    so.customer_name = customer_name
+    so.set_warehouse = mbc_default_po_warehouse
+    so.po_no = po.name
+    so.po_date = po.transaction_date
+    so.company = mbc_internal_company
+    so.payment_terms_template = payment_terms
+    so.tax_category = tax_category
+    so.taxes_and_charges = tax_temp_name
+    so.tc_name = terms_name
+    so.delivery_date = po.schedule_date
+    so.docstatus = 0
+    so.inter_company_order_reference = ""
+    so.terms = terms_description
+    so.flags.ignore_permissions = ignore_permissions
+    so.run_method("set_missing_values")
+    
     # set Items Table
-    def update_item(obj, target, source_parent):
-        target.warehouse = mbc_default_po_warehouse
-        if obj.against_blanket_order:
-            target.against_blanket_order = obj.against_blanket_order
-            target.blanket_order = obj.blanket_order
-            target.blanket_order_rate = obj.blanket_order_rate
-
-    # Doc Data mapped
-    doclist = get_mapped_doc(
-        "Purchase Order",
-        source_name,
-        {
-            "Purchase Order": {"doctype": "Sales Order", "validation": {"docstatus": ["=", 1]}},
-            "Purchase Order Item": {
-                "doctype": "Sales Order Item",
-                "postprocess": update_item,
-                "condition": lambda doc: doc.qty > 0,
-            },
-            "Sales Team": {"doctype": "Sales Team", "add_if_empty": True},
-        },
-        target_doc,
-        set_missing_values,
-        ignore_permissions=ignore_permissions,
-    )
-    doclist.set_payment_schedule()
-    doclist.set_taxes()
-    # postprocess: fetch shipping address, set missing values
-    doclist.set_onload("ignore_price_list", True)
-
-    return doclist
+    for item in po.items:
+        if item.qty > 0:
+            so_item = so.append('items', {})
+            so_item.item_code = item.item_code
+            so_item.item_name = item.item_name
+            so_item.description = item.description
+            so_item.qty = item.qty
+            so_item.stock_uom = item.uom
+            so_item.price_list_rate = item.rate
+            so_item.rate = item.rate
+            so_item.amount = flt(item.qty) * flt(item.rate)
+            so_item.warehouse = mbc_default_po_warehouse
+            so_item.delivery_date = po.schedule_date
+            if item.against_blanket_order:
+                so_item.against_blanket_order = item.against_blanket_order
+                so_item.blanket_order = item.blanket_order
+                so_item.blanket_order_rate = item.blanket_order_rate
+    
+    
+    so.set_taxes()
+    so.save(ignore_permissions=ignore_permissions)
+    frappe.msgprint(msg='Sales Order Created Successfully',
+                                title='Message',
+                                indicator='green')
+   
